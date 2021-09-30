2104b1c260deb06101ea1f75e3cd25a1905afd50
Philip <87368971+optiphil@users.noreply.github.com>
2021-09-30 14:38:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,11 +25,38 @@
            </div>
         <div class="search">
             <p>Definitions</p>
-            <input type="search">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"/></svg>
+            <input class="inputSearch" type="search">
+            <svg class="btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"/></svg>
        
         </div>
      
     </div>
+    <script>
+        let searchMe = document.querySelector(".inputSearch")
+        let btn = document.querySelector(".btn")
+        
+        
+        const getEvent = async()=>{
+            let dynamic = await searchMe.value;
+            const res = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${dynamic}?key=3488cc2c-650d-49f6-a197-08113e68d74b`)
+            const data =await res.json();
+
+           if(data){
+               data.map(e => {
+                   console.log(e.shortdef[0])
+                   return e.shortdef
+               })
+           }
+        }
+
+        
+        btn.addEventListener("click", () => {
+            getEvent()
+        })
+
+        
+
+    </script>
 </body>
 </html>
+
